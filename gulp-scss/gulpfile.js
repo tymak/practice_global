08146da6622dc90125715bda3121ebd17f084fa9
unknown <timko.michal@centrum.cz>
2019-05-06 14:21:42 +0200

    prubezny update

--- gulp-scss/gulpfile.js
+++ gulp-scss/gulpfile.js
@@ -1,21 +1,116 @@
-// load modules
+// load 
+const del = require('del');
+const gulp = require("gulp");
+const browsersync_server = require('browser-sync').create();
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 
-const gulp = require('gulp');
+// const del = require('del');
 
- 
+// delete all assets in dist
 
-// define functions
+// compile SCSS files
 
-function sayHello(done) { // sayHello is the name of the task
+function cleanAssets(done) {
 
-    console.log('Hello World'); // this just prints 'Hello world'
+    return del(
 
-    done();
+        ['dist/**/*.html', 'dist/fonts/**/*', 'dist/img/**/*'], 
+
+        { force: true }
+
+    );
 
 }
 
  
 
-// export tasks
+// publish HTML files src into dist
 
-exports.default = sayHello;
+// publish HTML files src into dist
+
+function publishHtml(done) {
+
+    return gulp.src('src/**/*.html')
+  
+      .pipe(htmlmin({ collapseWhitespace: true }))
+  
+      .pipe(gulp.dest('dist'));
+  
+  }
+  
+   
+  
+  // watch files
+  
+  function watchFiles(done) {
+  
+    gulp.watch("src/**/*.html", gulp.series(publishHtml, reload));
+    gulp.watch("src/scss/**/*.scss", gulp.series(compileScss, reload));}
+ 
+
+// Copy all fonts from src/fonts into dist
+
+function publishFonts(done) {
+
+    return gulp.src('src/fonts/**/*')
+
+      .pipe(gulp.dest('dist/fonts'));
+
+}
+
+ 
+
+// Copy all images from src/img into dist
+
+function publishImages(done) {
+
+  return gulp.src('src/img/**/*')
+
+    .pipe(gulp.dest('dist/img'));
+
+}
+
+// browserSync server
+
+function serve(done) {
+
+    browsersync_server.init({
+
+        server: {
+
+            baseDir: './dist/'
+
+        }
+
+    });
+
+    done();
+
+}
+// compile SCSS files
+
+
+
+// browserSync reload
+
+function reload(done) {
+
+    browsersync_server.reload();
+
+    done();}
+
+
+function compileScss(done) {
+
+        return gulp.src('src/scss/**/*.scss')
+      
+          .pipe(sass().on('error', sass.logError))
+      
+          .pipe(gulp.dest('dist/css'));
+    }
+
+
+
+exports.publish = gulp.series(cleanAssets, publishHtml, publishFonts, publishImages); //series-...proste obalí seznam funkcí
+exports.kotatko_hlida = gulp.series(cleanAssets, publishHtml,compileScss,serve, watchFiles);
